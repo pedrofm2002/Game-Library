af0be8516567b5dff355a7df444fae36135884a9
pedrof2002 <pedroferrarimartinez12@gmail.com>
2024-11-14 13:53:26 -0300

    Game Library on Storage

---
+++ components/Form.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import PropTypes from "prop-types"
+import TextInput from "./TextInput"
+
+NewGameForm.propTypes = {
+  addGame: PropTypes.func
+}
+
+export default function NewGameForm({ addGame }) {
+  const [title, setTitle] = useState("")
+  const [cover, setCover] = useState("")
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    addGame({ title, cover })
+    setTitle("")
+    setCover("")
+  }
+
+  return (
+		<form onSubmit={handleSubmit}>
+       <TextInput id="title" label="Título" value={title} onChange={(ev) => setTitle(ev.target.value)} />
+       <TextInput id="cover" label="Capa" value={cover} onChange={(ev) => setCover(ev.target.value)} />
+      <button>Adicionar</button>
+    </form>
+  )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,24 @@
-import { useState } from "react"
+
+import NewGameForm from "../components/NewGameForm"
+import Game from "../components/Game"
+import useGameCollection from "../hooks/useGameCollection"
 
 function App() {
-  const [games, setGames] = useState(() => {
-    const storedGames = localStorage.getItem("obc-game-lib")
-    if (!storedGames) return []
-    return JSON.parse(storedGames)
-  })
-
-  const [title, setTitle] = useState("")
-  const [cover, setCover] = useState("")
-
-  const addGame = ({ title, cover }) => {
-    const id = Math.floor(Math.random() * 1000000)
-    const game = { id, title, cover }
-    setGames(state => {
-      const newState = [...state, game]
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
-
-  const removeGame = (id) => {
-    setGames(state => {
-      const newState = state.filter(game => game.id !== id)
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
-
-  const handleSubmit = (ev) => {
-    ev.preventDefault()
-    addGame({ title, cover })
-    setTitle("")
-    setCover("")
-  }
+  const { games, addGame, removeGame } = useGameCollection()
 
   return (
     <div className="app">
       <h1>Biblioteca de Jogos</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Título:</label>
-          <input type="text" id="title" value={title} onChange={(ev) => setTitle(ev.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="cover">Capa:</label>
-          <input type="text" id="cover" value={cover} onChange={(ev) => setCover(ev.target.value)} />
-        </div>
-        <button>Adicionar</button>
-      </form>
+      <NewGameForm addGame={addGame} />
       <div className="games">
         {games.map((game) => (
           <div key={game.id}>
-            <img src={game.cover} alt="Capa do jogo" />
-            <div>
-              <h2>{game.title}</h2>
-              <button onClick={() => removeGame(game.id)}>
-                Remover
-              </button>
-            </div>
+            <Game
+            key={game.id}
+            title={game.title}
+            cover={game.cover}
+            onRemove={() => removeGame(game.id)}
+          />
           </div>))}
       </div>
     </div>
